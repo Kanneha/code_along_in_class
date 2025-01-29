f123eaeba0293e94c6a9f3e8e1c98677f767507c
KannehaLakshmi <pji.kanneha.lakshmi@kalvium.community>
2025-01-29 15:45:34 +0530

    milestone_10

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,8 @@
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+import Home from "./pages/Home";
+import CreateProduct from './pages/createProduct.jsx';
+export {LoginPage,SignupPage,Home,CreateProduct};
+
+
+
